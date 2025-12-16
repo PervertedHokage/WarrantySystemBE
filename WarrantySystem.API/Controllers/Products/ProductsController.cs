de5609ff12edb66870190e2f6beab99779788e3c
ADMIN <ADMIN@DESKTOP-I8MS0CC>
2025-12-16 17:25:23 +0700

    Tạo API CRUD sản phẩm

---
+++ WarrantySystem.API/Controllers/Products/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using WarrantySystem.Model.Entities;
+using WarrantySystem.Repository.IRepositories;
+using WarrantySystem.Shared.Common;
+
+namespace WarrantySystem.API.Controllers.Products
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IGenericRepo _repo;
+
+        public ProductsController(IGenericRepo repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("get-products")]
+        public async Task<IActionResult> GetProducts()
+        {
+            try
+            {
+                var products = (await _repo.FindByExpression<Product>(x => x.IsDeleted == false));
+                return Ok(new
+                {
+                    status = 1,
+                    data = products
+
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ApiResponseFactory.Fail(ex, ex.Message));
+
+            }
+        }
+
+        [HttpPost("save-product")]
+        public async Task<IActionResult> SaveData([FromBody] Product products)
+        {
+            try
+            {
+                // Check trùng mã sản phẩm
+                var existed = (await _repo.FindByExpression<Product>(x => x.Code == products.Code
+                     && x.Id != products.Id && x.IsDeleted == false));
+
+                if (existed.Any())
+                {
+                    return Ok(new
+                    {
+                        status = 0,
+                        message = "Mã sản phẩm đã tồn tại."
+                    });
+                }
+
+                if (products.Id <= 0)
+                {
+                    await _repo.Insert(products);
+                }
+                else
+                {
+                    await _repo.Update(products);
+                }
+
+                return Ok(new
+                {
+                    status = 1,
+                    message = "Lưu thành công."
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ApiResponseFactory.Fail(ex, ex.Message));
+            }
+        }
+
+    }
+}
--- WarrantySystem.Model/Entities/Product.cs
+++ WarrantySystem.Model/Entities/Product.cs
@@ -22,4 +22,6 @@ public partial class Product
     public DateTime? UpdatedDate { get; set; }
 
     public string UpdatedBy { get; set; }
+    public bool IsDeleted { get; set; } = false;
+
 }
