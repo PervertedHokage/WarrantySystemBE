fb2ec14643c03d0f7f2668241381fb420b14ae12
ADMIN <ADMIN@DESKTOP-I8MS0CC>
2025-12-24 17:16:41 +0700

    Update linh kiện

--- WarrantySystem.API/Controllers/Products/ProductsController.cs
+++ WarrantySystem.API/Controllers/Products/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Xml.Linq;
+using WarrantySystem.Model.DTO;
 using WarrantySystem.Model.Entities;
+using WarrantySystem.Model.Param;
 using WarrantySystem.Repository.IRepositories;
 using WarrantySystem.Shared.Common;
 
@@ -37,42 +39,183 @@ namespace WarrantySystem.API.Controllers.Products
             }
         }
 
-        [HttpPost("save-product")]
-        public async Task<IActionResult> SaveData([FromBody] Product products)
+        [HttpGet("get-spare-parts-group")]
+        public async Task<IActionResult> GetSparePartGroups()
         {
             try
             {
-                // Check trùng mã sản phẩm
-                var existed = (await _repo.FindByExpression<Product>(x => x.Code == products.Code
-                     && x.Id != products.Id && x.IsDeleted == false));
-
-                if (existed.Any())
-                {
-                    return Ok(new
-                    {
-                        status = 0,
-                        message = "Mã sản phẩm đã tồn tại."
-                    });
-                }
-
-                if (products.Id <= 0)
-                {
-                    await _repo.Insert(products);
-                }
-                else
-                {
-                    await _repo.Update(products);
-                }
-
+                var data = (await _repo.FindByExpression<SparePartsGroup>(x => x.IsDeleted == false));
                 return Ok(new
                 {
                     status = 1,
-                    message = "Lưu thành công."
+                    data = data
+
                 });
             }
             catch (Exception ex)
             {
                 return BadRequest(ApiResponseFactory.Fail(ex, ex.Message));
+
+            }
+        }
+
+        [HttpPost("get-spare-parts")]
+        public async Task<IActionResult> GetListIssues([FromBody] SparePartsParam request)
+        {
+            try
+            {
+                var sparePart = await _repo.ProcedureToList<dynamic>("spGetSpareParts",
+                    new string[] { "@ProductId" },
+                    new object[] { request.ProductId });
+                return Ok(new
+                {
+                    status = 1,
+                    data = new
+                    {
+                        asset = sparePart
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ApiResponseFactory.Fail(ex, ex.Message));
+            }
+        }
+
+        [HttpGet("get-unit")]
+        public async Task<IActionResult> GetUnits()
+        {
+            try
+            {
+                var units = (await _repo.FindByExpression<Unit>(x => x.IsDeleted == false));
+                return Ok(new
+                {
+                    status = 1,
+                    data = units
+
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ApiResponseFactory.Fail(ex, ex.Message));
+
+            }
+        }
+
+
+        [HttpPost("save-data-product")]
+        public async Task<IActionResult> SaveDataProduct([FromBody] sparePartGroupDTO dto)
+        {
+            try
+            {
+                var claims = User.Claims.ToDictionary(x => x.Type, x => x.Value);
+                CurrentUser currentUser = ObjectMapper.GetCurrentUser(claims);
+
+                if (dto == null || dto.Product == null)
+                {
+                    return BadRequest(new { status = 0, message = "Dữ liệu không hợp lệ" });
+                }
+
+                int ProductID = 0;
+
+                // Master
+                if (dto.Product.Id <= 0)
+
+                {
+                    await _repo.Insert(dto.Product);
+                    ProductID = dto.Product.Id;
+                }
+                else
+                {
+                    await _repo.Update(dto.Product);
+                    ProductID = dto.Product.Id;
+                }
+
+                // Xử lý xóa chi tiết trong group này (nếu edit)
+                if (dto.DeletedSparePartGroup.Count > 0)
+                {
+                    foreach (var item in dto.DeletedSparePartGroup)
+                    {
+                        var deletedSparePartGroup = await _repo.GetById<SparePartsGroup>(item);
+                        if (deletedSparePartGroup != null)
+                        {
+                            deletedSparePartGroup.IsDeleted = true;
+                            await _repo.Update(deletedSparePartGroup);
+                        }
+                    }
+                }
+
+                foreach (var groupWithDetails in dto.SparePartsGroups)
+                {
+                    var groupDTO = groupWithDetails.SparePartsGroup;
+                    int groupId;
+                    groupDTO.ProductId = ProductID;
+                    // Lưu group1
+                    if (groupDTO.Id <= 0)
+                    {
+                        await _repo.Insert(groupDTO);
+                        groupId = groupDTO.Id;
+                    }
+                    else
+                    {
+                       var group = await _repo.GetById<SparePartsGroup>(groupWithDetails.SparePartsGroup.Id);
+                        if (group == null) continue; // an toàn
+                        await _repo.Update(groupDTO);
+                        groupId = group.Id;
+                    }
+
+                    // Lưu chi tiết của group này
+                    foreach (var detailDto in groupWithDetails.SparePart)
+                    {
+                        detailDto.ProductId = ProductID;
+                        detailDto.SparePartGroupId = groupId;
+                        var existing = (await _repo.FindByExpression<SparePart>(x => x.Id == detailDto.Id));
+
+                        if (existing == null || detailDto.Id <= 0)
+                        {
+                            await _repo.Insert(detailDto);
+                        }
+                        else
+                        {
+                            // Update detail
+                            var existingDetail = await _repo.GetById<SparePart>(detailDto.Id);
+                            if (existingDetail != null && existingDetail.SparePartGroupId == groupId)
+                            {
+                                await _repo.Update(detailDto);
+                            }
+                        }
+                    }
+                    // Xử lý xóa chi tiết trong group này (nếu edit)
+                    if (groupWithDetails.DeletedSparePart != null && groupWithDetails.DeletedSparePart.Any())
+                    {
+                        foreach (var item in groupWithDetails.DeletedSparePart)
+                        {
+                            var deletedSparePart = await _repo.GetById<SparePart>(item);
+                            if (deletedSparePart != null)
+                            {
+                                deletedSparePart.IsDeleted = true;
+                                await _repo.Update(deletedSparePart);
+                            }
+
+                        }
+                    }
+                }
+
+                return Ok(new
+                {
+                    status = 1,
+                    message = "Lưu thành công",
+                    id = ProductID,
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    status = 0,
+                    message = ex.Message,
+                    error = ex.ToString()
+                });
             }
         }
 
---
+++ WarrantySystem.Model/DTO/sparePartDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarrantySystem.Model.Entities;
+
+namespace WarrantySystem.Model.DTO
+{
+    public class sparePartGroupDTO
+    {
+        public Product Product { get; set; }
+        public List<SparePartsGroupWithDetailsDTO> SparePartsGroups { get; set; }
+        public List<int> DeletedSparePartGroup { get; set; }
+    }
+    public class SparePartsGroupWithDetailsDTO
+    {
+        public SparePartsGroup SparePartsGroup { get; set; }
+        public List<SparePart> SparePart { get; set; }
+        public List<int> DeletedSparePart { get; set; }
+    }
+
+
+}
